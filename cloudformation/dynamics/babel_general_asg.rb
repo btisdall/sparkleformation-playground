2dffb84fe2aa6cfd33c8d1d76447f01eec9b6a7e
bentis <ben@tisdall.org.uk>
2015-04-13 20:30:06 +0200

    Commit WIP

---
+++ cloudformation/dynamics/babel_general_asg.rb
@@ -0,0 +1,47 @@
+require 'base64'
+
+SparkleFormation.dynamic(:babel_general_asg) do |_name, _config = {}|
+
+  mappings.region_to_az_map do
+    _set('us-east-1'._no_hump,    :zones => %w(a b c d e).map { |x| "us-east-1#{x}" })
+    _set('us-west-1'._no_hump,    :zones => %w(a b c).map { |x|     "us-west-1#{x}" })
+    _set('us-west-2'._no_hump,    :zones => %w(a b c).map { |x|     "us-west-2#{x}" })
+    _set('eu-west-1'._no_hump,    :zones => %w(a b c).map { |x|     "eu-west-1#{x}" })
+    _set('eu-central-1'._no_hump, :zones => %w(a b c).map { |x|  "eu-central-1#{x}" })
+  end
+
+  resources(_name.to_sym) do
+    type 'AWS::AutoScaling::AutoScalingGroup'
+    properties do
+      availability_zones _cf_map(:region_to_az_map, 'AWS::Region', 'zones'._no_hump)
+      launch_configuration_name _config[:launch_configuration_name]
+      tags _array(
+        -> {
+          key 'Environment'
+          value ref!(:Environment)
+          propagate_at_launch true
+        },
+        -> {
+          key 'Role'
+          value ref!(:Role)
+          propagate_at_launch true
+        },
+        -> {
+          key 'Department'
+          value 'web'
+          propagate_at_launch true
+        },
+        -> {
+          key 'Name'
+          value join!('bentis', ref!(:Role), ref!(:Environment), 'other', :options => { :delimiter => '-' })
+          propagate_at_launch true
+        },
+        -> {
+          key 'Project'
+          value 'babel'
+          propagate_at_launch true
+        },
+      )
+    end
+  end
+end
--- cloudformation/templates/simple_app_stack.rb
+++ cloudformation/templates/simple_app_stack.rb
@@ -15,14 +15,19 @@ SparkleFormation.new('simple_app_stack').load(:base).overrides  do
       description 'Role eg "web", "cache"'
       type 'String'
     end
+    instance_type do
+      description 'Instance type eg "m3.large"'
+      type 'String'
+    end
   end
 
-  dynamic!(:web_and_api_launch_config, 'bentis_test', :public_ips => true, :security_groups => ['sg-efd5ac8b'])
+  dynamic!( :babel_general_launch_config, :web_launch_config, :instance_type => ref!(:instance_type), :security_groups => ['sg-efd5ac8b'])
+  dynamic!( :babel_general_asg, :web_asg, :launch_configuration_name => ref!(:web_launch_config))
 
   outputs do
     instance_id do
       description 'Name of the lauch configuration'
-      value _cf_ref(:launch_config)
+      value _cf_ref(:web_launch_config)
     end
   end
 end
